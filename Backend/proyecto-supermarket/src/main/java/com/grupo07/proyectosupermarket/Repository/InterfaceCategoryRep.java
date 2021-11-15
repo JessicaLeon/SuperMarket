7cba0540868fe2c1200ad53ebc71bdfa84af7452
Jessica León <82897654+JessicaLeon@users.noreply.github.com>
2021-11-14 22:17:58 -0600

    Delete Backend directory

--- Backend/proyecto-supermarket/src/main/java/com/grupo07/proyectosupermarket/Repository/InterfaceCategoryRep.java
+++
@@ -1,7 +0,0 @@
-package com.grupo07.proyectosupermarket.Repository;
-
-import com.grupo07.proyectosupermarket.Model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterfaceCategoryRep extends JpaRepository<Category, Integer> {
-}
--- Backend/proyecto-supermarket/src/main/java/com/grupo07/proyectosupermarket/Repository/InterfaceProductoRep.java
+++
@@ -1,7 +0,0 @@
-package com.grupo07.proyectosupermarket.Repository;
-
-import com.grupo07.proyectosupermarket.Model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterfaceProductoRep extends JpaRepository<Producto, Integer> {
-}
